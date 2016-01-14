871e01830c5f0167bc59ee24e246a3b2f7252d14
Kris Burke <kris.burke611@gmail.com>
2016-01-14 14:17:35 -0500

    Added someInstance.size and someInstance.enqueue to functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,20 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function(){
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
