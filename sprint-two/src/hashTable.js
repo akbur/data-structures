dfdeeaa2f30b4035bd6315777f3b40d2b529fab0
Kris Burke <kris.burke611@gmail.com>
2016-01-16 21:54:54 -0500

    Fixed implementation of hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,53 +6,25 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  //result holds {k1:v1, k2:v2}
-  //[[k1, v1], [k2, v2]]
 
-  //get and save storage at i
-  // if undefined or null
-  //set storage[i] {k, v}
-  //else 
-  //storage[i][k] = v;
+  var result = this._storage.get(i);
 
-
-
-  //check storage at index
-  this._storage.each(function(item, index, storage) {
-    //if index is empty
-    console.log(storage[i]);
-
-    if(storage[i] === undefined || storage[i] === null) {
-      //add [{k: v}] to the storage at that index
-      this._storage.set(i, {k: v});
-    //if there's an object in that index   
-    } else {
-      var contains = false;
-      // go through the objects 
-      _.each(storage[i], function(obj) {
-        //if the object contains the key
-        if (_.contains(obj, k)) {
-          contains = true;
-          //overwrite the value
-          obj[k] = v;
-        }
-      });
-      // if no key equals k
-      if (!contains) {
-        //push the new object to the array
-        storage[i].push({k: v});
-      }
+  if (result === undefined || result === null) {
+    this._storage.set(i, {});
+  }
+  this._storage.each(function(obj, index, storage){
+    if (index === i) {
+      obj[k] = v;
+      console.dir(obj);
     }
-    console.log(storage[i]);
   });
-
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var result = this._storage.get(i);
   if(result !== undefined && result !== null) {
-    return result.k;
+    return result[k];
   } else {
     return result;
   }
